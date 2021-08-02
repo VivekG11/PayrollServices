e01b30b060a83320787e5ffa07db94755e60e0cf
VivekG11 <vivekvk2903@gmail.com>
2021-08-02 17:15:08 +0530

    [Creating a table in a database]

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -3,3 +3,11 @@ create database Payrool_Services;
 use payroll_serivce;
 --------UC1--------------
 
+create table Employe_Payroll
+(
+id int identity(1,1),
+name varchar (100) not null,
+salary float,
+startDate date
+)
+-------uc2--------
