28da8d128f0125b175c1f531ee9f4d18a715f5f6
VivekG11 <vivekvk2903@gmail.com>
2021-08-02 17:25:06 +0530

    [retrieving  data from table]

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -14,6 +14,8 @@ startDate date
 
 insert into Employe_Payroll values('Vivek',45432,'1999-06-11'),('Pavani',65784,'2000-02-17'),('Kumar',34562,'2002-09-12'),('Aditya',23511,'2000-09-21');
 
-select * from Employe_Payroll;
 
 --------UC3-------------------
+
+select * from Employe_Payroll;
+---------UC4-----------------
