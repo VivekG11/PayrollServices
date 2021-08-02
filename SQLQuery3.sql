c14e1405787c9b2732947087eb98659c44feaeb3
VivekG11 <vivekvk2903@gmail.com>
2021-08-02 17:22:30 +0530

    [Adding data into tables]

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -11,3 +11,9 @@ salary float,
 startDate date
 )
 -------uc2--------
+
+insert into Employe_Payroll values('Vivek',45432,'1999-06-11'),('Pavani',65784,'2000-02-17'),('Kumar',34562,'2002-09-12'),('Aditya',23511,'2000-09-21');
+
+select * from Employe_Payroll;
+
+--------UC3-------------------
